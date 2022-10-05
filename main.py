95c8f2411f3168d1189bc296d4898ff9905a4014
PimLeerkes <83347609+PimLeerkes@users.noreply.github.com>
2022-10-05 16:06:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 
-#vertaald woord op basis van richting in talen en beschikbare woordenboek
-def vertaal_woord(woord, richting, woorden):
-    vertaald_woord = ""
-    for i, j in enumerate(woorden):
-        if str.upper(j) == str.upper(woord):
-            if int(richting) == 1:
-                vertaald_woord = woorden[i+1]
-            elif int(richting) == 2:
-                vertaald_woord = woorden[i-1]
-    return vertaald_woord
+class Woordenboek(dict):
+
+    def __init__(self):
+        self = dict()
+
+    # voeg woord toe:
+    def add(self, key, value):
+        self[str.upper(key)] = value
+
+    # laat alle woorden zien:
+    def show(self):
+        print(self)
+
+    # vertaalt woord:
+    def vertaal(self, key):
+        if str.upper(key) in self:
+            return self[str.upper(key)]
+        else:
+            return key
 
 
 # iterate door de zin en vertaald elk woord los van elkaar:
-def vertaal_zin(zin, richting, woorden):
+def vertaal_zin(zin, woorden):
     zin = zin.split()
     # split de zin eerst in mogelijke subzinnen
     #subzinnen = []
@@ -36,7 +45,7 @@ def vertaal_zin(zin, richting, woorden):
 
     nieuwe_zin = ""
     for woord in zin:
-        nieuw_woord = vertaal_woord(woord, richting, woorden)
+        nieuw_woord = woorden.vertaal(woord)
         if nieuw_woord != "":
             woord = nieuw_woord
         nieuwe_zin = nieuwe_zin + " " + woord
@@ -44,13 +53,14 @@ def vertaal_zin(zin, richting, woorden):
 
 
 def main():
-    print("Welkom bij straattaalvertaler versie 0.0.3!")
+    print("Welkom bij straattaalvertaler versie 0.0.4!")
     print("Maak geen spellingsfouten bij het vertalen maar hoofdletters maken niet uit.")
     print("")
 
-    #laad de woorden in een list (woordenboek):
+    #laad de woorden in 2 woordenboeken. 1 van nederlands naar straattaal en 1 andersom.:
     woorden = open("woorden.txt", "r")
     woorden = woorden.readlines()
+    ned_str, str_ned = Woordenboek(), Woordenboek()
     res = []
     for w in woorden:
         woord = w.replace("\n", "")
@@ -62,19 +72,32 @@ def main():
         if woord != "" and woord[len(woord)-1] == " ":
             woord = woord[:-1]
         res.append(woord)
+    for i, j in enumerate(res):
+        if i % 2 == 1:
+            ned_str.add(res[i], res[i+1])
+            str_ned.add(res[i+1], res[i])
 
-    #vraag om woorden te vertalen en print resultaat:
+
+    # vraag om woorden te vertalen en print resultaat:
     while(True):
         richting = input("Wil je van nederlands naar straattaal? (1) of van straattaal naar nederlands? (2): ")
+        if richting == "1":
+            woordenlijst = ned_str
+        elif richting == "2":
+            woordenlijst = str_ned
+        else:
+            print("Vul 1 of 2 in alstjeblieft. Dit mag niet!!!")
+            continue
+
         zin = input("Welk woord of zin wil je vertalen?: ")
-        vertaalde_woord = vertaal_zin(zin, richting, res)
+        vertaalde_zin = vertaal_zin(zin, woordenlijst)
         print("")
-        #print(vertaalde_woord)
-        print("De vertaling voor: '" + zin + "' is: " + vertaalde_woord)
+        print("De vertaling voor: '" + zin + "' is: " + vertaalde_zin)
         print("")
 
 
 if __name__ == '__main__':
     main()
+
 
 
