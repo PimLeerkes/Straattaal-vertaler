b29e4d136ea116c9d5cd7ad7ada16f5db479b061
brightly <theintegrative@gmail.com>
2022-10-10 16:22:59 +0200

    Little fixed

--- app/api.py
+++ app/api.py
@@ -1,9 +1,17 @@
 from fastapi import BackgroundTasks, FastAPI, Query
-import vertaler
+import uvicorn
+import sys
+sys.path.append('../')
+import app.vertaler as vertaler
+
 
 ### How to run from cli -> uvicorn api:app --reload
 
 app = FastAPI()
+
+def run_me():
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
+
 
 @app.get("/")
 async def root():
